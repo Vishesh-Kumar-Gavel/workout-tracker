eb4dbd4a10a5097ae47bfb129204583aa63df472
icyhitman <icyhitman@Icyhitmans-MacBook-Air.local>
2025-08-07 02:51:59 +0530

    saved changes in Exercise class?

--- src/main/java/com/icyhitman/workouttracker/entity/Exercise.java
+++ src/main/java/com/icyhitman/workouttracker/entity/Exercise.java
@@ -1,5 +1,22 @@
 package com.icyhitman.workouttracker.entity;
 
-public class Exercise {
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Exercise {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	private Set<String> muscleGroups = new HashSet<>();
 }
